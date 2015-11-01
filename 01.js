b176da96d86a64f7dccc7edcceb0d9b778c804f3
Lucas <lucas@agtlucas.com>
2015-11-01 08:48:44 -0200

    Update 01.js

---
+++ 01.js
@@ -0,0 +1,10 @@
+// For
+function getStockSymbols(stocks) {
+
+}
+
+var symbols = getStockSymbols([
+  { symbol: 'XFX', price: 240.22, volume: 23432 },
+  { symbol: 'TNZ', price: 332.19, volume: 234 },
+  { symbol: 'JXJ', price: 162.59, volume: 5323 }
+]);
